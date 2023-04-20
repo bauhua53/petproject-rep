84bc2ccd07492d310b736b75166fb5d7d9f8b4fc
bauhua53 <131352475+bauhua53@users.noreply.github.com>
2023-04-20 12:52:17 +0300

    Create main.rs
    Combination of Rust's built-in random number generator and the standard library's IO functionalities

---
+++ main.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let mut rng = rand::thread_rng();
+    let mut script = String::new();
+
+    for _ in 0..10 {
+        let num: u8 = rng.gen();
+        script.push_str(&format!("println!(\"Random number: {}\");\n", num));
+    }
+
+    let mut file = File::create("random_script.rs")?;
+    file.write_all(script.as_bytes())?;
+
+    Ok(())
+}
